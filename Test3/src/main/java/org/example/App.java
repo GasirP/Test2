9f0792c67aed1e5d0b3f168d9a0a4665d522dd22
GasirP <gasir.pawel@gmail.com>
2022-11-26 15:48:29 +0100

    xs

--- Test3/src/main/java/org/example/App.java
+++ Test3/src/main/java/org/example/App.java
@@ -6,8 +6,35 @@ package org.example;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        Integer number = 2;
+        if (number == 0 ) {
+            System.out.println("Zero");
+        }
+        else if (number == 1 ) {
+            System.out.println("One");
+        }
+        else if (number == 2 ) {
+            System.out.println("Two");
+        }
+        else if (number == 3 ) {
+            System.out.println("Three");
+        }
+switch (number) {
+    case 0:
+        System.out.println("Zero");
+        break;
+    case 1:
+        System.out.println("One");
+        break;
+    case 2:
+        System.out.println("Two");
+        break;
+    case 3:
+        System.out.println("Three");
+        break;
+}
+
     }
+
 }
